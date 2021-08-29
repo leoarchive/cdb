b3a237cfa87486ac0af06462d16a309b18904841
leozamboni <leonardonunes169@gmail.com>
2021-08-29 15:49:30 -0300

    update

--- database.c
+++ database.c
@@ -16,6 +16,7 @@
  *  along with this program.  if not, see <http://www.gnu.org/licenses/>.
  */ 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
 
@@ -24,13 +25,59 @@
 enum {
 	CREATE,
 	PRINT,
+	REMOVE,
 	DESTROY,
 };
 
-void destroy(FILE *f);
+typedef uint8_t boolean;
+
+void destroy(void);
+boolean removeId(FILE *f, size_t i);
 void print(FILE *f, char c);
 void create(FILE *f, char **argv);
-uint8_t data_access_object(FILE *f, int argc, char **argv, uint8_t cmd);
+boolean data_access_object(FILE *f, int argc, char **argv, uint8_t cmd);
+uint8_t get_cmd(const char **cmds, char *argv);
+
+int main(int argc,char **argv) 
+{
+	if (argc < 2) return 1;
+
+	const char *cmds[] = {"-c","-p","-r","-d",NULL};
+
+	FILE *f = fopen(DB,"a+");
+	if (!f) return 1; 
+
+	fflush(stdout);
+
+	boolean r = data_access_object(f,argc,argv,get_cmd(cmds,argv[1]));
+	if (r) return 1;
+		
+	return 0;
+}
+
+boolean data_access_object(FILE *f, int argc, char **argv, uint8_t cmd)
+{
+	switch (cmd) 
+	{
+		case CREATE:
+			if (argc < 4) return 1;
+			create(f,argv+2);
+			break;	
+		case PRINT:
+			print(f,fgetc(f));
+			break;	
+		case REMOVE:
+			if (argc < 3) return 1;
+			if (removeId(f,strtol(argv[2],NULL,10))) return 1;
+			break;
+		case DESTROY:
+			destroy();
+			break;			
+		default: return 1;
+	}
+	fclose(f);	
+	return 0;
+}
 
 uint8_t get_cmd(const char **cmds, char *argv)
 {
@@ -39,45 +86,57 @@ uint8_t get_cmd(const char **cmds, char *argv)
 	return 1 + get_cmd(cmds,argv);	
 }
 
-int main(int argc,char **argv) 
+size_t get_lines(FILE *f)
 {
-	if (argc < 2) return 1;
-
-	const char *cmds[] = {"-c","-p","-d",NULL};
-
-	FILE *f = fopen(DB,"a+");
-	if (!f) return 1; 
-
-	fflush(stdout);
-
-	if (data_access_object(f,argc,argv,get_cmd(cmds,argv[1]))) return 1;
-		
-	return 0;
-}
-
-uint8_t data_access_object(FILE *f, int argc, char **argv, uint8_t cmd)
-{
-	switch (cmd) 
+	char c;
+	size_t cnt = 0;
+	do
 	{
-		case CREATE:
-			if (argc < 4) return 1;
-			create(f,argv);
-			break;	
-		case PRINT:
-			print(f,fgetc(f));
-			break;	
-		case DESTROY:
-			destroy(f);
-			break;			
-		default: return 1;
-	}
-	fclose(f);	
-	return 0;
+		c = fgetc(f);
+		if (c == '\n') cnt++;
+	} while (c != EOF);
+	return cnt;
 }
 
-void destroy(FILE *f)
+void destroy(void)
 {
 	remove(DB);
+}
+
+boolean removeId(FILE *f, size_t i)
+{
+	size_t lines = get_lines(f);
+	if (i > lines) return 1;
+
+	FILE *tmp = tmpfile();
+	if (!tmp) return 1;
+
+	rewind(f);
+
+	size_t cnt = 0;
+	char c;
+	while (c != EOF)
+	{		
+		if (c == '\n') cnt++;
+		c = fgetc(f);
+		if (cnt == i) continue;
+		fputc(c,tmp);
+	}
+
+	rewind(tmp);
+
+	fclose(f);
+	f = fopen(DB,"w");
+ 
+	while (1)
+	{
+		c = fgetc(tmp);
+		if (c==EOF) break;
+		fputc(c,f);
+	}
+
+	fclose(tmp);
+	return 0;
 }
 
 void print(FILE *f, char c)
@@ -89,6 +148,9 @@ void print(FILE *f, char c)
 
 void create(FILE *f, char **argv)
 {
-	fprintf(f,"\"%s\",",argv[2]);
-	fprintf(f,"\"%s\" \n",argv[3]);
+	if (!(*argv)) return;
+	fprintf(f,"\"%s\"",(*argv));
+	argv++;
+	fprintf(f,"%c",(!(*argv)?'\n':','));
+	create(f,argv);
 }
